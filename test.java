d4b5b36fde2232d2a5efba0a45194ca4f5b5083d
aThorp96 <andrew.thorp@protonmial.com>
2018-11-09 18:36:54 -0500

    added java file

---
+++ test.java
@@ -0,0 +1,10 @@
+import Jama.Matrix;
+
+public class test {
+
+    public static void main(String[] args ) {
+        double [][] array = {{1,2,3},{1,2,3},{1,2,3}};
+        Matrix m = new Matrix(array);
+    }
+
+}
