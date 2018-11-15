7992b9fb95ad4918409a87d035959019d62d5a0b
aThorp96 <andrewThorp@protonmail.com>
2018-11-14 20:25:00 -0500

    initialized a few files

--- test.java
+++ test.java
@@ -1,27 +1,38 @@
 
 import Jama.*;
+import java.util.Arrays;
 import java.awt.Color;
 
 public class test {
 
     public static void main(String[] args ) {
-        Matrix m = Matrix.random(400, 2);
-        int y = m.getColumnDimension();
-        int x = m.getRowDimension();
-        double[][] arr = m.getArray();
+        int y = 2;
+        int x = 40000;
+        double[][] arr = new double[40000][2];
+
+        int n = 0;
+
+        for (int i = 0; i < 200; i++) {
+            for (int j = 0; j < 200; j++) {
+                double[] tuple = {i * 2, j * 2};
+                arr[n] = tuple;
+                n++;
+            }
+        }
+
 
         Plotter plotter = Plotter.getPlotter();
         plotter.setName("Random Plotter");
 
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                plotter.plot(399 * arr[i][0], 399 * arr[i][1], (i % 2 == 0) ? Color.GREEN : Color.RED);
-                System.out.printf("Index [%d,%d]: %f\n", i, j, 400 * arr[i][j]);
+                plotter.plot(arr[i][0], arr[i][1], Color.RED);
+                System.out.printf("Index [%d,%d]: %f\n", i, j, arr[i][j]);
             }
         }
 
-
         plotter.refresh();
+
     }
 
 }
